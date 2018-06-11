1f9c51435b2ec68a5a616ed4f3aeda5a21df2b07
charleswang234 <wang.charles234@gmail.com>
2018-06-11 23:49:44 +0000

    implemented downloadImageByURL in download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var secret = require("./secrets.js");
+var fs = require('fs');
+
 
 console.log("Welcome to the Github Avatar Downloader");
 
@@ -17,13 +19,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  for (var i = 0; i < result.length; ++i) {
-    console.log(result[i]['avatar_url'])
-  }
-  // console.log("Errors:", err);
-  // console.log("Result:", result);
-});
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+          .on('response', function (response) {
+          })
+          .pipe(fs.createWriteStream(filePath));
+}
 
 
-//
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   for (var i = 0; i < result.length; ++i) {
+//     console.log(result[i]['avatar_url'])
+//   }
+//   // console.log("Errors:", err);
+//   // console.log("Result:", result);
+// });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
