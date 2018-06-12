2fc30cdaf472a0c45ad273697d00223ba13ecead
charleswang234 <wang.charles234@gmail.com>
2018-06-12 00:26:42 +0000

    modified callback function to make the avatars directory and also corrected spacing in download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -5,14 +5,15 @@ if (args[0] === undefined) {
 } else if (args[1] === undefined) {
   throw "You did not enter the repoName";
 }
+
 var request = require('request');
 var secret = require("./secrets.js");
 var fs = require('fs');
 
-
 console.log("Welcome to the Github Avatar Downloader");
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var options = {
     url: "https://api.github.com/repos/" + args[0] + "/" + args[1] + "/contributors",
     headers: {
@@ -20,12 +21,12 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'Authorization': secret.GITHUB_TOKEN
     }
   };
+
   request(options, function(err, res, body) {
     var data = JSON.parse(body);
     cb(err, data);
   });
 }
-
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
@@ -35,11 +36,12 @@ function downloadImageByURL(url, filePath) {
 }
 
 
+// invokes getRepoContributors function
 getRepoContributors("jquery", "jquery", function(err, result) {
   for (var i = 0; i < result.length; ++i) {
-    // if (!fs.existsSync('./avatars')){
-    //   fs.mkdirSync('./avatars');
-    // }
+    if (!fs.existsSync('./avatars')){
+      fs.mkdirSync('./avatars');
+    }
     var currentFilePath = 'avatars/' + result[i]['login'] + ".jpg";
     downloadImageByURL(result[i]['avatar_url'], currentFilePath);
 
